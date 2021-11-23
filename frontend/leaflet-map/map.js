e1d27a71a3d6ebc15264aa3218c072bae810a49c
DIA Tech <diatech@jasdia.com>
2021-11-23 13:41:04 -0800

    [FE] Adding INFO and LEGEND

--- frontend/leaflet-map/map.js
+++ frontend/leaflet-map/map.js
@@ -4,3 +4,59 @@ L.tileLayer('http://a.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: 'Map data &copy; OpenStreetMap contributors',
   maxZoom: 18
 }).addTo(map);
+
+
+///////////////////////////////////////////////////
+// Utility to return needed colors for a range
+function getColor(d) {
+  return d > 1000 ? '#800026' :
+         d > 500  ? '#BD0026' :
+         d > 200  ? '#E31A1C' :
+         d > 100  ? '#FC4E2A' :
+         d > 50   ? '#FD8D3C' :
+         d > 20   ? '#FEB24C' :
+         d > 10   ? '#FED976' :
+                    '#FFEDA0';
+}
+
+///////////////////////////////////////////////////
+// Custom Info Control Displaying Stats
+var info = L.control();
+
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+    this.update();
+    return this._div;
+};
+
+// method that we will use to update the control based 
+// on feature properties passed
+info.update = function (props) {
+    this._div.innerHTML = '<h4>Road Traffic Statistics</h4>' +  (props ?
+        '<b>' + props.name + '</b><br />' + props.density + ' people / mi<sup>2</sup>'
+        : 'Click a location');
+};
+
+info.addTo(map);
+
+///////////////////////////////////////////////////
+// Legends
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 10, 20, 50, 100, 200, 500, 1000],
+        labels = [];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+};
+
+legend.addTo(map);
