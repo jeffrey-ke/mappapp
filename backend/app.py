f85626d958037f87dfeb2a771d1c2f2571b18055
Jeffrey Ke <jeffke613@gmail.com>
2021-11-28 11:10:52 -0800

    Added error handling. If the returns httpStatusCode is anything else other than success (200), return a default api call.

--- backend/app.py
+++ backend/app.py
@@ -22,9 +22,10 @@ def api():
         lng = float(request.args['lng'])
     else:
         return "Error: Not all fields given"
-        
-    r = requests.get('https://api.tomtom.com/traffic/services/4/flowSegmentData/relative0/10/json?point=' + str(lat) + '%2C' + str(lng) + '&key=' + key).json()
 
+    r = requests.get('https://api.tomtom.com/traffic/services/4/flowSegmentData/relative0/10/json?point=' + str(lat) + '%2C' + str(lng) + '&key=' + key).json()
+    if r['httpStatusCode'] != 200:
+        return jsonify({"morning": 0, "afternoon": 0, "evening": 0})
     speed = r["flowSegmentData"]["currentSpeed"]
     result = {"morning": speed, "afternoon": speed, "evening": speed}
     return jsonify(result)
