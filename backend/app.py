1ccc79c1c986918e7f226136df1e098bd663cc80
Jeffrey Ke <jeffke613@gmail.com>
2021-11-28 11:02:43 -0800

    Added the ability to do an API Call with the 'requests' package. Using tomtom's live traffic data API. Unfortunately tomtom's API does not offer traffic data other than current traffic data, so accessing other day's traffic data is impossible.

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,18 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import random
+import requests
 
 import json
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
 
+key = 'ZBZ4eiytk908RyTjNkoHinzopH4PN4ID'
+
 @app.route('/')
 def home():
     return "This is an API for traffic data"
 
-#this is the api call: here is where I receive coordinates, and return a json of the actual traffic data
 @app.route('/mappapp', methods=['GET'])
 def api():
     val = ('dow' in request.args) and ('lat' in request.args) and ('lng' in request.args)
@@ -20,14 +22,11 @@ def api():
         lng = float(request.args['lng'])
     else:
         return "Error: Not all fields given"
+        
+    r = requests.get('https://api.tomtom.com/traffic/services/4/flowSegmentData/relative0/10/json?point=' + str(lat) + '%2C' + str(lng) + '&key=' + key).json()
 
-    #morning will be 10am
-
-    #afternoon will be 2pm
-
-    #evening will be 6pm
-
-    result = {"morning": random.randint(0,550), "afternoon": random.randint(0,550), "evening": random.randint(0,550)}
+    speed = r["flowSegmentData"]["currentSpeed"]
+    result = {"morning": speed, "afternoon": speed, "evening": speed}
     return jsonify(result)
 
 if __name__ == '__main__':
